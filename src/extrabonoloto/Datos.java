49824bd0ca89a385e667c78604e73079bef1b62d
menuven <hposecarames.Hector Pose Carames>
2019-06-03 21:33:48 +0200

    Finalizado

---
+++ src/extrabonoloto/Datos.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package extrabonoloto;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Hector Pose Carames
+ */
+public class Datos {
+    ArrayList<String> numeros = new ArrayList<>();
+
+    public ArrayList<String> getNumeros() {
+        return numeros;
+    }
+
+    public void setNumeros(ArrayList<String> numeros) {
+        this.numeros = numeros;
+    }
+
+    public Datos() {
+    }
+
+    
+    
+}
---
+++ src/extrabonoloto/ExtraBonoloto.java
@@ -0,0 +1,18 @@
+
+package extrabonoloto;
+
+/**
+ *
+ * @author Hector Pose Carames
+ */
+public class ExtraBonoloto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Bonoloto obx = new Bonoloto();
+        obx.setVisible(true);
+    }
+    
+}
